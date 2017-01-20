fee21b70a1146e2952da3a987c9076204573a459
Adam Luxton <adze_202@hotmail.com>
2017-01-20 14:13:11 -0500

    Index page updated.
    Index page almost fully styled. Time sensitive functionality added.

--- admin/phpscripts/login.php
+++ admin/phpscripts/login.php
@@ -1,10 +1,11 @@
 <?php
+  date_default_timezone_set('America/Toronto');
 
   function logIn($username, $password, $ip){
     require_once("connect.php");
     $username = mysqli_real_escape_string($link, $username);
     $password = mysqli_real_escape_string($link, $password);
-    $loginString = "SELECT * FROM tbl_user WHERE user_name='{$username}' AND user_pass = '{$password}'";
+    $loginString = "SELECT * FROM tbl_users WHERE user_name='{$username}' AND user_password = '{$password}'";
     //echo $loginString;
     $user_set = mysqli_query($link, $loginString);
 
@@ -13,15 +14,27 @@
       $id = $found_user['user_id'];
       $_SESSION['users_id'] = $id;
       $_SESSION['users_name'] = $found_user['user_name'];
+      $_SESSION['users_login'] = $found_user['user_lastLogin'];
       if(mysqli_query($link, $loginString)){
-        $updateString = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
-        $updateQuery = mysqli_query($link, $updateString);
+        $updateIPString = "UPDATE tbl_users SET user_ip='{$ip}' WHERE user_id={$id}";
+        $newDate = date("jS \\of F, Y");
+        $updateLoginString = "UPDATE tbl_users SET user_lastLogin='{$newDate}' WHERE user_id={$id}";
+        $updateQuery = mysqli_query($link, $updateIPString);
+        $updateLoginQuery = mysqli_query($link, $updateLoginString);
       }
       redirect_to("index.php");
     }
     else{
-      $message = "Username/Password were incorrect. <br>Please make sure your caps-lock key is turned off.";
-      return $message;
+      $accountString = "SELECT user_id FROM tbl_users WHERE user_name = '{$username}'";
+      $accountExists = mysqli_query($link, $accountString);
+      if(mysqli_num_rows($accountExists)){
+
+      }
+      else{
+        $message = "Username/Password were incorrect. <br>Please make sure your caps-lock key is turned off.";
+        return $message;
+      }
+
     }
     mysqli_close($link);
   }
